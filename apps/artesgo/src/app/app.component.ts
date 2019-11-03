5847a1fd077407ec30b6d114bd26ff56f2cfba10
artesgo <artesgo@artesgos-MacBook-Pro.local>
2019-11-03 09:43:12 -0500

    gsap animations

--- apps/artesgo/src/app/app.component.ts
+++ apps/artesgo/src/app/app.component.ts
@@ -8,6 +8,8 @@ import * as gsap from 'gsap';
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('gsapEl') gsapEl: ElementRef;
+  @ViewChild('rotato') rotato: ElementRef;
+
   title = "artesgo";
   options = [
     {
@@ -32,6 +34,7 @@ export class AppComponent implements AfterViewInit {
     },
   ];
   slide = 0;
+  shown = true;
 
   constructor() {
   }
@@ -42,9 +45,46 @@ export class AppComponent implements AfterViewInit {
     // })
   }
 
+  animateMe() {
+    const box: Element = this.gsapEl.nativeElement;
+    const duration = 1.3;
+    if (this.shown) {
+      this.shown = false;
+      gsap.TweenMax
+        .to(box, duration, { ease: gsap.Bounce.easeOut, height: 32, width: 32, 'background-color': 'rbga(0, 255, 0, 1)' })
+    } else {
+      this.shown = true;
+      gsap.TweenMax
+        .to(box, duration, { ease: gsap.Bounce.easeOut, height: 1, width: 1, 'background-color': 'rbga(0, 0, 0, 1)' })
+        .eventCallback('onComplete', () => {
+          console.log('onComplete');
+        })
+    }
+  }
+
+  popIn() {
+    const leaf: Element = this.rotato.nativeElement;
+    const duration = 1.3;
+
+    gsap.TweenMax
+      .set(leaf, {
+        rotation: "0"
+      })
+    gsap.TweenMax
+      .to(leaf, duration, {
+        ease: gsap.Linear.easeNone,
+        rotation: "360",
+      })
+      .eventCallback('onComplete', () => {
+        // gsap.TweenMax
+        //   .to(leaf, duration, { ease: gsap.Linear.easeNone, transform: 'rotate(6.2rad)' })
+      })
+  }
+  getRotation(index: number) {
+    return `rotate( ${index*45} 16  16)`;
+  }
+
   prevDefault(event: Event) {
     event.preventDefault();
-    console.log('prevDefault');
-    
   }
 }
--- libs/e11y/src/lib/radio/radio.component.ts
+++ libs/e11y/src/lib/radio/radio.component.ts
@@ -1,20 +1,88 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChildren, QueryList, ElementRef, AfterViewInit } from '@angular/core';
 import { E11yOption } from './e11y.option';
+import * as gsap from 'gsap';
 
 @Component({
   selector: 'e11y-radio',
   templateUrl: './radio.component.html',
   styleUrls: ['./radio.component.scss']
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements AfterViewInit {
   @Input() groupLabel: string = "radio group";
   @Input() options: E11yOption[] = [];
+  @Output() focus = new EventEmitter();
+  @ViewChildren('pettles') pettles: QueryList<ElementRef>;
+  @ViewChildren('radios') radios: QueryList<ElementRef>;
+  unit = 16;
+  offset = 6;
 
   height = 32;
   width = 32;
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    for( let flower of this.pettles.toArray()) {
+      this.setDefault(flower.nativeElement);
+    }
+  }
+
+  setDefault(flower: Element) {
+    gsap.TweenMax
+      .set(flower, {
+        ...this.scaledDown()
+      })
+  }
+
+  focused(option: E11yOption, index: number) {
+    this.focus.emit(option);
+    
+    const pettle: Element = this.pettles.toArray()[index].nativeElement;
+    const radio: Element = this.radios.toArray()[index].nativeElement;
+    const duration = 1.3;
+    gsap.TweenMax
+      .to(pettle, duration, {
+        ease: gsap.Bounce.easeOut,
+        transform: 'translate( 0px, 0px ) scale(1)',
+      })
+    gsap.TweenMax
+      .set(radio, {
+        rotation: '0',
+      })
+    gsap.TweenMax
+      .to(radio, duration, {
+        ease: gsap.Bounce.easeOut,
+        rotation: '180',
+      })
+      // .eventCallback('onComplete', () => {
+      //   console.log('onComplete');
+      // })
+  }
+
+  scaledDown() {
+    const scale = (this.unit - this.offset) / this.unit;
+    const _default = {
+      transform: `translate( ${this.offset}px, ${this.offset}px ) scale(${scale})`,
+    };
+    return _default;
+  }
+
+  blurred(option: E11yOption, index: number) {
+    const leaf: Element = this.pettles.toArray()[index].nativeElement;
+    const duration = 1.3;
+    gsap.TweenMax
+      .to(leaf, duration, {
+        ease: gsap.Bounce.easeOut,
+        ...this.scaledDown()
+      })
+      // .eventCallback('onComplete', () => {
+      //   console.log('onComplete');
+      // })
+
+  }
+
+  getRotation(index: number) {
+    return `rotate( ${index*45} 16  16)`;
+  }
 }
