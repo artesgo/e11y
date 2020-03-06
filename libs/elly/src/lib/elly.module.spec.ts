05612cb646f99aacdd0965b70c75fa5dafa89966
artesgo <artesgo@artesgos-MacBook-Pro.local>
2020-03-05 23:03:25 -0500

    chore: cleanup project config

--- libs/elly/src/lib/elly.module.spec.ts
+++ libs/elly/src/lib/elly.module.spec.ts
@@ -1,14 +1,14 @@
 import { async, TestBed } from '@angular/core/testing';
-import { EllyCompModule } from './elly.module';
+import { EllyModule } from './elly.module';
 
 describe('EllyCompModule', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [EllyCompModule],
+      imports: [EllyModule],
     }).compileComponents();
   }));
 
   it('should create', () => {
-    expect(EllyCompModule).toBeDefined();
+    expect(EllyModule).toBeDefined();
   });
 });
