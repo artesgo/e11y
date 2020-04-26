1ac8096426e144eb759e59d8303a068e9797573e
artesgo <artesgo@artesgos-MacBook-Pro.local>
2020-04-26 10:49:51 -0400

    fix: external story template

--- libs/elly/src/lib/radio/radio.component.stories.ts
+++ libs/elly/src/lib/radio/radio.component.stories.ts
@@ -2,11 +2,13 @@ import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { withA11y } from '@storybook/addon-a11y';
 
 import { RadioComponent } from './radio.component';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 storiesOf('Radio Button', module)
   .addDecorator(withA11y)
   .addDecorator(
     moduleMetadata({
+      imports: [ReactiveFormsModule],
       declarations: [ RadioComponent ],
     })
   )
@@ -19,12 +21,21 @@ storiesOf('Radio Button', module)
     ></e11y-radio>`,
     props: {
       players: players,
-      onClick: event => {
-        console.log('some bindings work');
-        console.log(event);
-      },
+      onClick: event => console.log(event),
     },
-  }));
+  }))
+  .add('turtles radio control', () => ({
+    templateUrl: "story-templates/radio-in-form.html",
+    props: {
+      turtles: turtles,
+      turtlesControl
+    },
+  }))
+
+const turtlesControl = new FormControl();
+turtlesControl.valueChanges.subscribe(val => {
+  console.log(val);
+});
 
 const players = [
   { label: '2 Players', name: 'playerCount', value: '2 Players' },
@@ -33,3 +44,8 @@ const players = [
   { label: '5 Players', name: 'playerCount', value: '5 Players' },
   { label: '6 Players', name: 'playerCount', value: '6 Players' },
 ];
+const turtles = [
+  { label: 'Snapping Turtle', name: 'turtles', value: 'Snapping Turtle' },
+  { label: 'Sea Turtle', name: 'turtles', value: 'Sea Turtle' },
+  { label: 'Pond Turtle', name: 'turtles', value: 'Pond Turtle' },
+]
